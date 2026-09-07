56ba64c7f28156e2d1da02cfce7c68c2b8c120f6
mardiyah <lailatulmardiyah939@gmail.com>
2026-09-07 11:12:29 +0700

    integrasi flutter firebase

--- deteksi wajah/lib/main.dart
+++ deteksi wajah/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:si_wajah/firebase_options.dart';
 
 late List<CameraDescription> cameras;
 
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp (
+    options : DefaultFirebaseOptions . currentPlatform , 
+);
+  
   cameras = await availableCameras();
 
   runApp(const AbsensiDinasApp());
